1f898056d1b3adcd0e49c82dcaa3b32ef8200518
fseeeye <fseeeye@gmail.com>
2022-01-18 16:50:08 +0800

    chore: update premake5 config

--- premake5.lua
+++ premake5.lua
@@ -1,42 +1,41 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-    staticruntime "off"
-
+	
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_tables.cpp",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp",
-		"backends/imgui_impl_opengl3.h",
+		"*.h",
+		"*.cpp",
+		"backends/imgui_impl_opengl3.*",
 		"backends/imgui_impl_opengl3_loader.h",
-		"backends/imgui_impl_glfw.h"
+		"backends/imgui_impl_glfw.*"
 	}
 
+	includedirs
+	{
+		".",
+		"backends",
+		"examples/libs/glfw/include"
+	}
+	
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
+		staticruntime "on"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
 		cppdialect "C++17"
+		staticruntime "on"
 
 	filter "configurations:Debug"
 		runtime "Debug"
-		symbols "on"
+		symbols "On"
 
 	filter "configurations:Release"
 		runtime "Release"
-		optimize "on"
+		optimize "On"
