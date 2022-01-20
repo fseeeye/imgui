743295feb122f47fca517b7993a23f5384edd3f6
fseeeye <fseeeye@gmail.com>
2022-01-20 13:30:39 +0800

    feat: update premake5 config

--- premake5.lua
+++ premake5.lua
@@ -1,41 +1,39 @@
 project "ImGui"
-	kind "StaticLib"
-	language "C++"
-	
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "On"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
-		"*.h",
-		"*.cpp",
-		"backends/imgui_impl_opengl3.*",
-		"backends/imgui_impl_opengl3_loader.h",
-		"backends/imgui_impl_glfw.*"
-	}
+    files
+    {
+        "*.h",
+        "*.cpp",
+        "backends/imgui_impl_opengl3.*",
+        "backends/imgui_impl_opengl3_loader.h",
+        "backends/imgui_impl_glfw.*"
+    }
 
-	includedirs
-	{
-		".",
-		"backends",
-		"examples/libs/glfw/include"
-	}
-	
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "on"
+    includedirs
+    {
+        ".",
+        "backends",
+        "examples/libs/glfw/include"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
 
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "on"
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "On"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "On"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
